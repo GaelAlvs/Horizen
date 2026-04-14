9a564cc7815697c723bc1534d803e4ed4d95394f
Gabriel <gabriel.alvs19@gmail.com>
2026-04-14 08:55:37 -0300

    Estruturação das rotas

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  searchTerm: string = '';
+  cartCount: number = 2; // depois virá de um serviço
+
+  onSearch() {
+    console.log('Buscando:', this.searchTerm);
+    // futuramente: redirecionar para página de busca
+  }
+
+  openMenu() {
+    console.log('Abrir menu lateral');
+  }
+
+  openCart() {
+    console.log('Abrir carrinho');
+  }
+}
